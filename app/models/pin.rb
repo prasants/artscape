fd6fb65ba3aa03c0fb738a05f16719d95f7c292a
Prasant Sudhakaran <prasant@prasant.net>
2014-07-12 01:55:19 +0530

    Added aws.rb and s3 details to pin.rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,6 +1,7 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :storage =>   
+:s3, :bucket => "artscape"
      validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
      validates :image, presence: true
 		 validates :description, presence: true
