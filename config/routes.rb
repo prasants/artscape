531ed11c39128b7ae6317c7dd4e4b3f38336c2ab
Prasant Sudhakaran <prasant@prasant.net>
2014-07-12 13:02:37 +0530

    Active Admin Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :views
+  devise_for :admins
   resources :pins
 
   devise_for :users
